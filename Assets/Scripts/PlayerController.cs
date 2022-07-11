8cda3b1b62495ebb11175de380c06a8926d55dea
Danny Schultz <danny@galliumstudios.com>
2022-07-11 13:18:05 -0400

    Roll
    By holding down while depressed melon can roll (sprites not added)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,22 @@ public class PlayerController : MonoBehaviour
 {
     private Meloncholy m_pia;
     private Rigidbody2D m_rb;
+    private CapsuleCollider2D m_cac;
+    private CircleCollider2D m_cic;
+
     private float x = 0;
     
     [SerializeField] private float moveSpeed = 6f;
     [SerializeField] private float jumpHeight = 15f;
-    [SerializeField] private float slashFloat = 5;
+    [SerializeField] private float slashFloat = 10;
+
+    private enum PlayerState {None, Denial, Anger, Bargaining, Depression, Acceptance}
+    [SerializeField] PlayerState playerState = PlayerState.None;
+
+    [SerializeField] private float slashCooldownTime = .5f;
 
     private bool canMove = true;
+    private bool crouching = false;
 
     public Meloncholy PIA => m_pia;
 
@@ -41,6 +50,9 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         m_rb = GetComponent<Rigidbody2D>();
+        m_cac = GetComponent<CapsuleCollider2D>();
+        m_cic = GetComponent<CircleCollider2D>();
+
         m_pia.Player.Jump.started += Jump;
         m_pia.Player.Paint.started += PaintAction;
         m_pia.Player.Enable();
@@ -54,7 +66,30 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         x = m_pia.Player.Movement.ReadValue<Vector2>().x;
-        if (canMove)
+        if (playerState == PlayerState.Depression)
+        {
+            if (!crouching)
+            {
+                if (m_pia.Player.Movement.ReadValue<Vector2>().y < 0)
+                {
+                    if (checkGround() && canMove)
+                    {
+                        Crouch(true);
+                    }
+                }
+            }
+            else
+            {
+                if (m_pia.Player.Movement.ReadValue<Vector2>().y >= 0)
+                {
+                    if (checkStandRoom() && canMove)
+                    {
+                        Crouch(false);
+                    }
+                }
+            }
+        }
+        if (canMove && !crouching)
         {
             transform.Translate(Vector2.right * x * Time.deltaTime * moveSpeed);
         }
@@ -67,6 +102,17 @@ public class PlayerController : MonoBehaviour
             m_rb.velocity *= Vector2.right;
             m_rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
         }
+    }
+
+    private void Crouch(bool shouldCrouch)
+    {
+        crouching = shouldCrouch;
+        m_cac.enabled = !shouldCrouch;
+        m_cic.enabled = shouldCrouch;
+        m_rb.freezeRotation = !shouldCrouch;
+        m_rb.velocity *= .6f;
+        transform.eulerAngles = shouldCrouch ? transform.eulerAngles : Vector3.zero;
+        canSlash = !shouldCrouch;
     }
 
     private void PaintAction(InputAction.CallbackContext context)
@@ -85,7 +131,7 @@ public class PlayerController : MonoBehaviour
     IEnumerator slashCoolDown ()
     {
         canSlash = false;
-        yield return new WaitForSeconds(.5f);
+        yield return new WaitForSeconds(slashCooldownTime);
         canSlash = true;
     }
 
@@ -106,6 +152,26 @@ public class PlayerController : MonoBehaviour
         else
         {
             return false;
+        }
+    }
+
+    private bool checkStandRoom ()
+    {
+        RaycastHit2D[] hits = Physics2D.BoxCastAll(new Vector2(transform.position.x, transform.position.y + .4f), new Vector2(.9f, .1f), 0, Vector2.up, 1.2f);
+        if (hits.Length != 0)
+        {
+            for (int i = 0; i < hits.Length; ++i)
+            {
+                if (hits[i].collider.gameObject != this.gameObject)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        else
+        {
+            return true;
         }
     }
 }
