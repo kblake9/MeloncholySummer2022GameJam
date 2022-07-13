11db47e5bc8ae2772ee14ac09814b82b98129811
Ryan <ryanabusa8134@gmail.com>
2022-07-13 12:36:41 -0400

    Added baseplat Mainmenu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Start()
+    {
+        
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
