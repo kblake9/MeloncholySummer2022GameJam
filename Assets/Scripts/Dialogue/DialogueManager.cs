854882dcfce2c4375846805ebcc5c072b6362d78
kblake9 <53315512+kblake9@users.noreply.github.com>
2022-07-12 21:26:04 -0500

    tweaked dialogue box text speed to be slower

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -65,7 +65,7 @@ public class DialogueManager : MonoBehaviour
         foreach (char letter in sentence.ToCharArray())
         {
             dialogueText.text += letter;
-            yield return null;
+            yield return new WaitForSeconds(0.02f);
         }
     }
 
