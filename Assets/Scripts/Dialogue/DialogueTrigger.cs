6bd75e39a4b6b71b12bc6ea8ebe36aede8567ad2
Ryan <ryanabusa8134@gmail.com>
2022-07-16 19:15:32 -0400

    sdfasd

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -4,24 +4,13 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    public bool isColliding;
     private GameObject gameObject;
     public Dialogue[] dialogues;
     public bool isEndScene;
     [SerializeField] private string SceneName;
 
-    void Start()
-    {
-        isColliding = true;
-    }
-    
-
     public void TriggerDialogue()
     {
-        if (isColliding) {
-            Debug.Log("How many times is this being called?");
-            DialogueManager.Instance.QueueDialogue(dialogues, SceneName, isEndScene);
-            isColliding = false;
-        }
+           DialogueManager.Instance.QueueDialogue(dialogues, SceneName, isEndScene);
     }
 }
